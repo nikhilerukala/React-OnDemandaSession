9d5281d9ffc53bc6e2a81269255da8efec5961a3
nikhilerukala <nikhilerukala99@gmail.com>
2022-12-06 08:17:19 +0000

    solution of onDemandSession

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,27 @@
+import './index.css'
+import {on} from 'events'
+
+const TabItem = props => {
+  const {tabDetails, updateActiveTabId, isActive} = props
+  const {tabId, displayText} = tabDetails
+
+  const activeTabClassName = isActive ? `active-tab-btn` : ``
+
+  const onClickTabItem = () => {
+    updateActiveTabId(tabId)
+  }
+
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabClassName}`}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
